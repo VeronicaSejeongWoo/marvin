b0d4429800ccf7cb1de3cf956809747edc5fe0a7
VeronicaSejeongWoo <veronica.woo@geappliances.com>
2019-01-10 20:04:49 +0900

    Added key control

--- aduino_robotarm/aduino_robotarm.ino
+++ aduino_robotarm/aduino_robotarm.ino
@@ -1,12 +1,16 @@
 #include <Servo.h>
 
 #define REG_MAX 1023
+#define REG_MAX1 734
+
 #define POS_MAX 180
+
+#define ARM_REG_CHANNEL1 3
 
 typedef struct
 {
   int count;
-  int posArm[100];
+  int posArm[180];
 }ServoData;
 
 enum
@@ -26,11 +30,12 @@ void SetServoControl(Servo *servo, int position)
 {
   position = min(position, POS_MAX);
   servo->write(position);
+  Serial.println(position);
 }
 
 int GetArmPosition(int regInput)
 {
-  int armPosition = map(regInput, 0, REG_MAX, 0, POS_MAX);
+  int armPosition = map(regInput, 0, REG_MAX1, 0, POS_MAX);
   return armPosition;
 }
 
@@ -38,60 +43,113 @@ void SaveArmPosition(ServoData *data, int channel, int count)
 {
   int regValue = analogRead(channel);
   data->posArm[count] = GetArmPosition(regValue);
-//  Serial.println(regValue);
-  Serial.println(data->posArm[count]);
 }
+
+void UpdateCount(int savedCount)
+{
+  count++;
+  
+  if(controlState == STATE_RECORD)
+  {
+    count = min(count, 100);
+    savedCount = count;
+  }
+  else if(controlState ==  STATE_PLAY)
+  {
+    if(count >= savedCount)
+    {
+      count = 0;
+    }    
+  }
+}
+
+void pciSetup(byte pin)
+{
+  *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
+  PCIFR |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+  PCICR |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+}
+
+ISR (PCINT2_vect)
+{
+  if(digitalRead(2) == 1)
+  {
+    digitalWrite(7, LOW);
+    controlState = STATE_STOP;
+    Serial.println("Stop");
+  }
+
+  if(digitalRead(3) == 1)
+  {
+    digitalWrite(7, LOW);
+    controlState = STATE_RECORD;
+    Serial.println("Record");
+//    digitalWrite(7, HIGH);
+//    controlState = STATE_PLAY;  
+//    Serial.println("Play");
+  }
+
+  if(digitalRead(4) == 1)
+  {
+    digitalWrite(7, HIGH);
+    controlState = STATE_PLAY;  
+    Serial.println("Play");
+  }
+}
+
+int pos;
 
 void setup() {
   // put your setup code here, to run once: 
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+
+  // Relay
+  pinMode(7, OUTPUT);
+  digitalWrite(7, LOW);
+
+  pciSetup(2);
+  pciSetup(3);
+  pciSetup(4);
+  
   Serial.begin(9600); 
-  servo01.attach(5);
+  servo01.attach(6);
   delay(20);
 
-  servoData01.count = 100;
-  for(int i=0; i<100; i++)
+  servoData01.count = 180;
+  for(int i=0; i<180; i++)
   {
-    servoData01.posArm[i] = 0;
+    servoData01.posArm[i] = i;
   }
 
   count = 0;
-  controlState = STATE_RECORD;
-  Serial.println("Record Started!");
+  controlState = STATE_STOP;
+  Serial.println("RobotArm Started!");
 }
 
 void loop() {
+  int tempValue;
   // put your main code here, to run repeatedly:
   
   switch(controlState)
   {
     case STATE_STOP:
+//    tempValue = analogRead(3);
+//    Serial.println(tempValue);
+//    delay(500);
     break;
 
     case STATE_RECORD:
-    Serial.println(count);
-    SaveArmPosition(&servoData01, 3, count);
+    SaveArmPosition(&servoData01, ARM_REG_CHANNEL1, count);
+    UpdateCount(servoData01.count);
     delay(200);
-    count++;
-    if(count == 50)
-    {
-      count = 0;
-      controlState = STATE_PLAY;
-      Serial.println("Play!");
-    }
     break;
 
     case STATE_PLAY:
     SetServoControl(&servo01, servoData01.posArm[count]); 
-    Serial.println(servoData01.posArm[count]);
-    delay(100);
-    count++;
-    
-    if(count == 100)
-    {
-      count = 0;
-//      controlState = STATE_STOP;
-      Serial.println("PLAY again!");
-    }
+    UpdateCount(servoData01.count);
+    delay(15);
     break;
     
     default:
