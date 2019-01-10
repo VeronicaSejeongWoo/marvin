cbf72cda8b6b0ef0e475fec0e554c57aed46368d
VeronicaSejeongWoo <veronica.woo@geappliances.com>
2019-01-10 13:40:03 +0900

    Updated

--- aduino_robotarm/aduino_robotarm.ino
+++ aduino_robotarm/aduino_robotarm.ino
@@ -3,9 +3,18 @@
 #define REG_MAX 780
 #define POS_MAX 180
 
+enum
+{
+  STATE_STOP = 0,
+  STATE_RECORD,
+  STATE_PLAY
+};
+
 Servo servo01;
 int servo01Pos;
 int posArm01;
+
+int controlState;
 
 void SetServoControl(Servo *servo, int position)
 {
@@ -26,10 +35,27 @@ void setup() {
   // put your setup code here, to run once:  
   servo01.attach(5);
   delay(20);
+
+  controlState = STATE_PLAY;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  posArm01 = GetArmPosition(analogRead(3));
-  SetServoControl(&servo01, posArm01); 
+  
+  switch(controlState)
+  {
+    case STATE_STOP:
+    break;
+
+    case STATE_RECORD:
+    break;
+
+    case STATE_PLAY:
+    posArm01 = GetArmPosition(analogRead(3));
+    SetServoControl(&servo01, posArm01); 
+    break;
+    
+    default:
+    break;
+  }
 }
