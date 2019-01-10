a9c17e439ad2afa3b2d5ef902ec5e41215a336f6
VeronicaSejeongWoo <veronica.woo@geappliances.com>
2019-01-10 15:38:14 +0900

    Recording and Playing

--- aduino_robotarm/aduino_robotarm.ino
+++ aduino_robotarm/aduino_robotarm.ino
@@ -1,7 +1,13 @@
 #include <Servo.h>
 
-#define REG_MAX 780
+#define REG_MAX 1023
 #define POS_MAX 180
+
+typedef struct
+{
+  int count;
+  int posArm[100];
+}ServoData;
 
 enum
 {
@@ -11,18 +17,15 @@ enum
 };
 
 Servo servo01;
-int servo01Pos;
-int posArm01;
+ServoData servoData01;
 
 int controlState;
+int count;
 
 void SetServoControl(Servo *servo, int position)
 {
-  if(position > POS_MAX)
-  {
-    position = POS_MAX;
-  }
-    servo->write(position);
+  position = min(position, POS_MAX);
+  servo->write(position);
 }
 
 int GetArmPosition(int regInput)
@@ -31,12 +34,29 @@ int GetArmPosition(int regInput)
   return armPosition;
 }
 
+void SaveArmPosition(ServoData *data, int channel, int count)
+{
+  int regValue = analogRead(channel);
+  data->posArm[count] = GetArmPosition(regValue);
+//  Serial.println(regValue);
+  Serial.println(data->posArm[count]);
+}
+
 void setup() {
-  // put your setup code here, to run once:  
+  // put your setup code here, to run once: 
+  Serial.begin(9600); 
   servo01.attach(5);
   delay(20);
 
-  controlState = STATE_PLAY;
+  servoData01.count = 100;
+  for(int i=0; i<100; i++)
+  {
+    servoData01.posArm[i] = 0;
+  }
+
+  count = 0;
+  controlState = STATE_RECORD;
+  Serial.println("Record Started!");
 }
 
 void loop() {
@@ -48,11 +68,30 @@ void loop() {
     break;
 
     case STATE_RECORD:
+    Serial.println(count);
+    SaveArmPosition(&servoData01, 3, count);
+    delay(200);
+    count++;
+    if(count == 50)
+    {
+      count = 0;
+      controlState = STATE_PLAY;
+      Serial.println("Play!");
+    }
     break;
 
     case STATE_PLAY:
-    posArm01 = GetArmPosition(analogRead(3));
-    SetServoControl(&servo01, posArm01); 
+    SetServoControl(&servo01, servoData01.posArm[count]); 
+    Serial.println(servoData01.posArm[count]);
+    delay(100);
+    count++;
+    
+    if(count == 100)
+    {
+      count = 0;
+//      controlState = STATE_STOP;
+      Serial.println("PLAY again!");
+    }
     break;
     
     default:
